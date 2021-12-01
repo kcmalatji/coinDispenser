8bc60cd255a8619976106706f08dc943f218d6cd
chris <f5161703@fnb.co.za>
2021-12-01 09:07:36 +0200

    added Junit tests

--- src/test/java/com/chris/interview/controller/CoinDispenserControllerTest.java
+++ src/test/java/com/chris/interview/controller/CoinDispenserControllerTest.java
@@ -1,126 +1,65 @@
 package com.chris.interview.controller;
 
-import java.util.Enumeration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
-@SuppressWarnings("deprecation")
-class CoinDispenserControllerTest {
-	CoinDispenserController coinDispenserController = new CoinDispenserController();
-	HttpSession session =new HttpSession() {
-		
-		@Override
-		public void setMaxInactiveInterval(int interval) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void setAttribute(String name, Object value) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void removeValue(String name) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void removeAttribute(String name) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void putValue(String name, Object value) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public boolean isNew() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public void invalidate() {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public String[] getValueNames() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Object getValue(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public HttpSessionContext getSessionContext() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public ServletContext getServletContext() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public int getMaxInactiveInterval() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public long getLastAccessedTime() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public String getId() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public long getCreationTime() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public Enumeration<String> getAttributeNames() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public Object getAttribute(String name) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	};;
-	//start server before running the test
-	@Test
-	void test() {
+import com.chris.impl.CoinDispencerImpl;
+import com.chris.model.CoinDispenserRequest;
+import com.google.gson.Gson;
 
-		int amount =36;
-		String denominators ="1,2,3,4,5,6";
-		coinDispenserController.getHome(amount, denominators, session);
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+class CoinDispenserControllerTest {
+	
+	CoinDispencerImpl coinDispencerImpl = new CoinDispencerImpl();
+	private final OkHttpClient httpClient = new OkHttpClient();
+	
+	@Test
+	void testcoinDispencerImpl() {
+		
+		int amount = 36;
+		int denominators[] = {1,2,3,4,5,7};
+		assertEquals(6, coinDispencerImpl.getMinimum(denominators, amount));
 	}
+	
+	@Test
+	void testcoinDispencerAPI() throws IOException {
+		
+		int amount = 36;
+		String denominators = "1,2,3,4,5,7";
+		CoinDispenserRequest request =new CoinDispenserRequest();
+		request.setAmount(amount);
+		request.setDenominators(denominators);
+		
+		Gson gs = new Gson();
+		gs.toJson(request);
+		 String  responseBody;
+		RequestBody body = RequestBody.create(MediaType.parse("application/json"), gs.toJson(request));
+
+		Request httpRequest = new Request.Builder().url("http://localhost:8080/v1/api/getcoins").post(body).build();
+
+		System.out.println("request " + request);
+		
+	     try (Response response = httpClient.newCall(httpRequest).execute()) {
+
+	            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+	            // Get response body
+	              responseBody=response.body().string();
+	     
+	            System.out.println("Minimum Combination is:" + responseBody);
+	            response.close();
+	        }
+	     
+		assertEquals(6,Integer.parseInt(responseBody));
+	}
+	
+	
 }
